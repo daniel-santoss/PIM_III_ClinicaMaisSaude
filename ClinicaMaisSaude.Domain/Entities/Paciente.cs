ffd32929188b46e718e3a0f1ed1ccd4a571ef8d2
Daniel Vinicius <danielvinicius.santos7@gmail.com>
2026-04-07 22:54:39 -0300

    Adicionar arquivos de projeto.

---
+++ ClinicaMaisSaude.Domain/Entities/Paciente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClinicaMaisSaude.Domain.Entities
+{
+
+    public class Paciente
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Cpf { get; private set; }
+        public string Telefone { get; private set; }
+            
+    public Paciente(string nome, string cpf, string telefone)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Cpf = cpf;
+            Telefone = telefone;
+        }
+    }
+}
+
