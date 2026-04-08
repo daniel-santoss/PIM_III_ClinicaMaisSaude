f2b87c66aebf609622ae7f7cd8d9b8958d3ee013
Daniel Vinicius <danielvinicius.santos7@gmail.com>
2026-04-08 20:43:15 -0300

    primeira migration do EF Core

---
+++ ClinicaMaisSaude.Infrastructure/Data/ClinicaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicaMaisSaude.Infrastructure.Data
+{
+    public class ClinicaDbContext : DbContext
+    {
+        // DbSet representa as tabelas do banco de dados
+        public DbSet<Domain.Entities.Paciente> Pacientes { get; set; }
+        public DbSet<Domain.Entities.Agendamento> Agendamentos { get; set; }
+
+        // Pacote de configuração do Entity Framework Core
+        public ClinicaDbContext(DbContextOptions<ClinicaDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ClinicaMaisSaude.Infrastructure/Migrations/20260408233915_CriacaoInicial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicaMaisSaude.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataHoraConsulta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProbabilidadeFalta = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PacienteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MedicoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
