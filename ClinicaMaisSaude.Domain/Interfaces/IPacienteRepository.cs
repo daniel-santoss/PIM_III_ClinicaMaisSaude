523a227ad52b0b0cbc62924bec56d57bb7d8e611
Daniel Vinicius <danielvinicius.santos7@gmail.com>
2026-04-08 22:05:55 -0300

    Criando as Interfaces

---
+++ ClinicaMaisSaude.Domain/Interfaces/IPacienteRepository.cs
@@ -0,0 +1,16 @@
+﻿using ClinicaMaisSaude.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicaMaisSaude.Domain.Interfaces
+{
+    public interface IPacienteRepository
+    {
+        // O "Async" no nome do método indica que ele é assíncrono e deve ser aguardado com "await" ao ser chamado
+        Task AdicionarAsync(Paciente paciente);
+        Task<Paciente?> ObterPorIdAsync(Guid id) ;
+        Task<Paciente?> ObterPorCpfAsync(string cpf);
+
+    }
+}
