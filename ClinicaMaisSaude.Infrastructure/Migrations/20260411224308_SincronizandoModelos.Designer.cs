39de1d0a8d51fe9598e527b2eff4bc350e0c2058
Daniel Vinicius <danielvinicius.santos7@gmail.com>
2026-04-11 20:05:11 -0300

    Criação do controller e configuração do program

--- ClinicaMaisSaude.Infrastructure/Migrations/20260411224308_SincronizandoModelos.Designer.cs
+++ ClinicaMaisSaude.Infrastructure/Migrations/20260411224308_SincronizandoModelos.Designer.cs
@@ -4,6 +4,7 @@ using ClinicaMaisSaude.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClinicaMaisSaude.Infrastructure.Migrations
 {
     [DbContext(typeof(ClinicaDbContext))]
-    partial class ClinicaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260411224308_SincronizandoModelos")]
+    partial class SincronizandoModelos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ClinicaMaisSaude.Infrastructure/Migrations/20260411224308_SincronizandoModelos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicaMaisSaude.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SincronizandoModelos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Pacientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Pacientes");
+        }
+    }
+}
